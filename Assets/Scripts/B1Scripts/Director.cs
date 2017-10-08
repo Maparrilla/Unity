0d528142dc3d932465ce6d468411a912a22fc8d4
Taichi Aritomo <taichiaritomo@gmail.com>
2017-10-07 22:25:12 -0400

    Controllable Obstacle

--- Assets/Scripts/B1Scripts/Director.cs
+++ Assets/Scripts/B1Scripts/Director.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 /* Handles Mouse Input for 
  */
@@ -11,19 +12,37 @@ public class Director : MonoBehaviour {
 
 	private List<NavMeshAgent> selectedAgents; // Currently selected NavMeshAgents
 	private List<NavMeshAgent> movingAgents;   // Currently moving NavMeshAgents
-	private List<NavMeshObstacle> selectedObstacles;
 	private List<NavMeshAgent> selectedNazguls;
+
+	private NavMeshObstacle selectedObstacle;
+	private Transform TargetObject;
+//	private Material selectedMaterial;
+//	private Material agentMaterial;
+//	private Material nazgulMaterial;
+//	private Material obstacleMaterial;
 
 	private Ray shootRay;        // Ray cast from mouse position (at camera)
 	private RaycastHit shootHit; // GameObject hit by shootRay
+
+	public enum KEYBOARD_INPUT : int
+	{
+		YAW_POS = KeyCode.RightArrow,
+		YAW_NEG = KeyCode.LeftArrow,
+		PITCH_POS = KeyCode.UpArrow,
+		PITCH_NEG = KeyCode.DownArrow,
+	}
 
 
 	// Use this for initialization
 	void Start () {
 		selectedAgents = new List<NavMeshAgent> ();
 		movingAgents = new List<NavMeshAgent> ();
-		selectedObstacles = new List<NavMeshObstacle> ();
 		selectedNazguls = new List<NavMeshAgent> ();
+
+//		selectedMaterial = Resources.Load ("Assets/Resources/B1Materials/PurpleWall") as Material;
+//		agentMaterial = Resources.Load ("B1Materials/Agent") as Material;
+//		nazgulMaterial = Resources.Load ("B1Materials/Nazgul") as Material;
+//		obstacleMaterial = Resources.Load ("B1Materials/BlueCylinder") as Material;
 	}
 
 	// Update is called once per frame
@@ -39,37 +58,40 @@ public class Director : MonoBehaviour {
 			{
 				if (hit.collider.CompareTag ("Agent"))
 				{
+//					GameObject myObject = hit.transform.gameObject;
+//					myObject.GetComponent<MeshRenderer> ().material = selectedMaterial;
+					selectedObstacle = null;
 					selectedAgents.Add (hit.transform.gameObject.GetComponent<NavMeshAgent> ());
 					Debug.Log ("Agent selected");
 				}
 
 				else if (hit.collider.CompareTag ("Obstacle"))
 				{
-					selectedObstacles.Add (hit.transform.gameObject.GetComponent<NavMeshObstacle> ());
+					selectedObstacle = hit.transform.gameObject.GetComponent<NavMeshObstacle> ();
+					TargetObject = hit.transform;
 					Debug.Log ("Obstacle selected");
 				}
 
 				else if (hit.collider.CompareTag ("Nazgul"))
 				{
+					selectedObstacle = null;
 					selectedNazguls.Add (hit.transform.gameObject.GetComponent<NavMeshAgent> ());
 					Debug.Log ("Nazgul selected");
 				}
 					
 				else // environment was selected
 				{
+					selectedObstacle = null;
+
 					foreach (NavMeshAgent agent in selectedAgents)
 					{
-						agent.destination = hit.point;
-						agent.Resume ();
-						movingAgents.Add (agent);
+						agent.SendMessage ("moveTo", hit.point);
+//						agent.destination = hit.point;
+//						agent.Resume ();
+//						movingAgents.Add (agent);
+//						agent.gameObject.GetComponent<MeshRenderer> ().material = agentMaterial;
 					}
 					selectedAgents.Clear (); // clear selection
-
-					foreach (NavMeshObstacle obstacle in selectedObstacles)
-					{
-						// move the obstacle
-					}
-					selectedObstacles.Clear (); // clear selection
 
 					foreach (NavMeshAgent naz in selectedNazguls)
 					{
@@ -82,39 +104,45 @@ public class Director : MonoBehaviour {
 		}
 
 
-
-
-		// To avoid bunching, check each agent's destination.
-		// If an agent's destination has another near-stationary agent in it,
-		// retarget the destination to a slightly closer one.
-		// (B1 - Refined Navigation: 1)
-		Vector3 offset_y = new Vector3 (0.0f, 1.0f, 0.0f); // check presence of other agents at an offset height of 1
-		foreach (NavMeshAgent agent in movingAgents)
+		if (selectedObstacle != null)
 		{
-			if (agent.isStopped)
-				movingAgents.Remove (agent);
+			float ObjectRotationSpeed = 5f;
+			Vector3 movement = new Vector3(0,0,0);
+//			float moveX = Input.GetAxis ("HorizontalArrow");
+//			float moveZ = Input.GetAxis ("VerticalArrow");
+//			Debug.Log (moveX);
+//			movement = movement + new Vector3 (moveX, 0, moveZ);
+//
+////			Vector3.RotateTowards (movement, Camera.main.transform.forward, 2 * Mathf.PI, 0.0F);
+//			movement.y = 0;
+//			Debug.Log (movement);
 
-			else
+			foreach (KEYBOARD_INPUT val in System.Enum.GetValues(typeof(KEYBOARD_INPUT)))
 			{
-				Collider[] hitColliders = Physics.OverlapSphere (agent.destination + offset_y, 0.1f);
-
-				// Check overlapping colliders
-				for (int i = 0; i < hitColliders.Length; i++)
-				{
-					NavMeshAgent other = hitColliders [i].gameObject.GetComponent<NavMeshAgent> ();
-
-					// If there is a near-stationary agent occupying the destination
-					if (other != null && other != agent && other.velocity.magnitude < 0.1f) {
-						Debug.Log ("Rerouting " + agent.gameObject.name);
-
-						// Redirect to a destination just short of the old one.
-						Vector3 opposite = agent.destination - agent.gameObject.transform.position;
-						agent.destination = agent.destination - (0.8f * opposite.normalized);
-						agent.Resume ();
+				if (Input.GetKey ((KeyCode)val)) {
+					switch (val) {
+					case KEYBOARD_INPUT.YAW_POS:
+//						TargetObject.Rotate (TargetObject.forward, -ObjectRotationSpeed * Time.deltaTime, Space.World);
+						movement = new Vector3 (1.0f, 0.0f, 0.0f);
+						break;
+					case KEYBOARD_INPUT.YAW_NEG:
+//						TargetObject.Rotate (TargetObject.forward, (ObjectRotationSpeed * Time.deltaTime), Space.World);
+						movement = new Vector3 (-1.0f, 0.0f, 0.0f);
+						break;
+					case KEYBOARD_INPUT.PITCH_POS:
+//						TargetObject.Rotate (TargetObject.right, ObjectRotationSpeed * Time.deltaTime, Space.World);
+						movement = new Vector3 (0.0f, 0.0f, 1.0f);
+						break;
+					case KEYBOARD_INPUT.PITCH_NEG:
+//						TargetObject.Rotate (TargetObject.right, -(ObjectRotationSpeed * Time.deltaTime), Space.World);
+						movement = new Vector3 (0.0f, 0.0f, -1.0f);
+						break;
 
 					}
 				}
 			}
+			movement = Camera.main.transform.TransformDirection (movement); // transform movement vector to correspond to camera angle
+			selectedObstacle.gameObject.SendMessage ("applyForce", movement);
 		}
 
 
---
+++ Assets/Scripts/B1Scripts/MovableObstacle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableObstacle : MonoBehaviour {
+
+	private Rigidbody rb;
+	private bool selected;
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = 10.0f;
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	}
+
+	void applyForce (Vector3 movement)
+	{
+		rb.AddForce (movement * speed);
+	}
+}
